5c36f8312912a68446738f16f2814b03789d0e83
Crystal Gong <crystalgong123@gmail.com>
2019-11-25 22:10:07 -0600

    updated package requirements

--- imports.py
+++ imports.py
@@ -2,6 +2,9 @@ import nltk
 from nltk.tokenize import casual_tokenize
 nltk.download('averaged_perceptron_tagger')
 
+from nltk.corpus import stopwords
+stopWords = set(stopwords.words('english'))
+
 import pandas as pd
 import numpy as np
 
@@ -17,5 +20,17 @@ warnings.filterwarnings('ignore')
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import chi2
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix
 
 print("package imports done")
